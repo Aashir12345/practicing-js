8a75ccd788e782c72db117aad2b1a068a5f9c28c
Aashir12345 <112748833+Aashir12345@users.noreply.github.com>
2023-11-25 15:09:15 +0000

    01variables.js done

---
+++ 01_basics/01variables.js
@@ -0,0 +1,10 @@
+const accountId = "144553"
+let accountEmail = "aashir@gmail.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+
+// accountId = "124477"// not allowed
+
+
+console.log(accountId); 
+console.table([accountId,accountEmail,accountPassword,accountCity]);
