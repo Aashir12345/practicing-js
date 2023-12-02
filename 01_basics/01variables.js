f21c663010ffd256f2cfd6287a9ffc1035cbc0d9
Aashir12345 <112748833+Aashir12345@users.noreply.github.com>
2023-12-02 11:49:03 +0000

    datatypes

--- 01_basics/01variables.js
+++ 01_basics/01variables.js
@@ -1,10 +1,13 @@
 const accountId = "144553"
 let accountEmail = "aashir@gmail.com"
-var accountPassword = "12345"
-accountCity = "Karachi"
+var accountPassword = "12345" /* var ko use nahin karna hai */ 
+accountCity = "Karachi" /* variable is tarah bhi use hosakta hai*/
+let accountState;
 
 // accountId = "124477"// not allowed
-
+accountEmail = "saleem@gmail.com"
+accountPassword = "12424"
+accountCity = "Lahore"
 
 console.log(accountId); 
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all codes as newer js version // 
+
+// alert(33+3 ) // we are using nodejs not browser // 
+console.log(3 
+    +  
+    3); // code readibility should be high
+
+console.log("aashir");
+
+let name = "aashir"
+let age = 21
+let isLoggedin = true
+
+/*
+number => 2 to power 53
+bigint 
+string => ""
+boolean => true/false
+null => standalone value
+undefined => value is not defined
+symbol => unique
+*/  
+
+/*
+object
+*/
+
+console.log(typeof undefined);
+console.log(typeof symbol);
+console.log(typeof null);
